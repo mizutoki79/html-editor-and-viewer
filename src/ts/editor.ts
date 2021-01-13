b05668fb9fdd5cbd702bd6d92564e52d268631a5
mizutoki79 <a.p9xelic5+git@gmail.com>
2021-01-14 07:14:15 +0900

    キーバインディングとローカルに保存処理を追加

--- src/ts/editor.ts
+++ src/ts/editor.ts
@@ -2,9 +2,38 @@ import * as monaco from 'monaco-editor';
 
 const prefersDarkTheme = () => window.matchMedia('(prefers-color-scheme: dark)').matches;
 
+const saveDocument = (editor: monaco.editor.ICodeEditor) => {
+    const doc = editor.getValue({ preserveBOM: false, lineEnding: '\n' });
+    chrome.storage.local.set({ index: doc });
+};
+
+const saveAndRender = (editor: monaco.editor.ICodeEditor) => {
+    console.log('save and render');
+    saveDocument(editor);
+    chrome.storage.local.get(['index'], items => {
+        console.log(items);
+    });
+};
+
+const extendCommandPalette = (editor: monaco.editor.IStandaloneCodeEditor) => {
+    const descriptor: monaco.editor.IActionDescriptor = {
+        id: 'saveAndRender',
+        label: 'Save and Render',
+        keybindings: [monaco.KeyMod.CtrlCmd | monaco.KeyCode.Enter],
+        contextMenuGroupId: '0',
+        contextMenuOrder: 0,
+        run: editor => saveAndRender(editor),
+    };
+    editor.addAction(descriptor);
+};
+
 const createEditor = (id: string, options: monaco.editor.IStandaloneEditorConstructionOptions) => {
     const editor = monaco.editor.create(document.getElementById(id), options);
+    extendCommandPalette(editor);
     window.onresize = () => editor.layout();
+    editor.onDidChangeCursorSelection(() => {
+        saveDocument(editor);
+    });
 };
 
 const main = () => {
