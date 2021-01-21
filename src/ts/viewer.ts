5a23a5c347e7d0322e7d15da07b6f3d0d35c20f5
s_maruyama <s_maruyama@yumemi.co.jp>
2021-01-22 00:05:38 +0900

    [WIP] js適用の試行錯誤を一旦コミット

--- src/ts/viewer.ts
+++ src/ts/viewer.ts
@@ -4,9 +4,34 @@ const appendStyle = (style: string) => {
     document.head.append(styleElement);
 };
 
+const appendScript = (script: string) => {
+    const scriptElement = document.createElement('script');
+    scriptElement.innerHTML = script;
+    document.body.appendChild(scriptElement);
+};
+
 chrome.storage.sync.get(['html', 'js', 'css'], items => {
     console.log(items);
     const container = document.getElementById('container');
     container.insertAdjacentHTML('afterbegin', items.html);
     appendStyle(items.css);
+    appendScript(items.js);
+    // const tabQueryOption: chrome.tabs.QueryInfo = {
+    //     currentWindow: true,
+    //     active: true,
+    //     lastFocusedWindow: true,
+    // };
+    // chrome.tabs.query(tabQueryOption, tabs => {
+    //     console.log('tabs: ', tabs);
+    //     console.log(tabs[0].id);
+    //     chrome.webNavigation.getAllFrames({ tabId: tabs[0].id }, details => {
+    //         console.log('details: ', details);
+    //         const scripts = items.js;
+    //         chrome.tabs.executeScript(tabs[0].id, {
+    //             allFrames: true,
+    //             frameId: details[0].frameId,
+    //             code: scripts,
+    //         });
+    //     });
+    // });
 });
