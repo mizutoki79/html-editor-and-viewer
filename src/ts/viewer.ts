38f8daf4ecf078fc2336c03d43cbb198ec00102f
s_maruyama <s_maruyama@yumemi.co.jp>
2021-01-21 20:03:37 +0900

    cssを適用するように実装

--- src/ts/viewer.ts
+++ src/ts/viewer.ts
@@ -1,5 +1,12 @@
+const appendStyle = (style: string) => {
+    const styleElement = document.createElement('style');
+    styleElement.textContent = style;
+    document.head.append(styleElement);
+};
+
 chrome.storage.sync.get(['html', 'js', 'css'], items => {
     console.log(items);
     const container = document.getElementById('container');
     container.insertAdjacentHTML('afterbegin', items.html);
+    appendStyle(items.css);
 });
